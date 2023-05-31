216d10e6fdc1407ef4857788767a39cbed3f2bd1
Maksim Kl <GuruHacker@yandex.ru>
2023-05-31 15:46:52 +0300

    Initial commit

---
+++ HomeWork-4.07.2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <list>
+#include <vector>
+
+template<class T>
+void print_container(const T& begin, const T& end) {
+    for (auto i = begin; i != end; i++) {
+        std::cout << *i << "  ";
+    }
+}
+
+int main()
+{
+    std::cout << "[SET]: ";
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set.cbegin(), test_set.cend()); // four one three two. помните почему такой порядок? :)
+    std::cout << std::endl << "[LIST]: ";
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list.cbegin(), test_list.cend()); // one  two  three  four
+    std::cout << std::endl << "[VECTOR]: ";
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector.cbegin(), test_vector.cend()); // one  two  three  four
+}
